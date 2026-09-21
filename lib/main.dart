de4dac5f83c6ea98696b46b600ce1c8a633a51f4
Julien92Joey <julien144@hotmail.fr>
2026-09-21 15:03:34 +0200

    Create main.dart for FuelApp with MapScreen
    Initialize the FuelApp with Material design and set MapScreen as the home.

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package0:flutter/material.dart';
+import 'screens/map_screen.dart';
+
+void main() {
+  runApp(const FuelApp());
+}
+
+class FuelApp extends StatelessWidget {
+  const FuelApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CarbuStock',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const MapScreen(),
+    );
+  }
+}
